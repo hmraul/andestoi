0f569ac8305419a062805dd82b0ef9555ed15370
Raul Herranz Munoz <raul.herranz@scmspain.com>
2021-06-20 11:06:37 +0200

    Creating use case class

---
+++ src/main/kotlin/com/rhm/andestoi/application/positions/Save.kt
@@ -0,0 +1,10 @@
+package com.rhm.andestoi.application.positions
+
+import com.rhm.andestoi.infrastructure.controller.PositionRequest
+import com.rhm.andestoi.infrastructure.controller.PositionResponse
+
+class Save {
+    fun execute(positionRequest: PositionRequest) : PositionResponse {
+        throw NotImplementedError()
+    }
+}
